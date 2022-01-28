5f7119cd973c8c8c4c20821ea5ef152bf96e4ca8
RobertoGarciaBetancourt <roberto.garcia@mercadolibre.com.co>
2022-01-28 18:36:52 -0500

    Start

---
+++ src/test/java/co/konradLorenz/synchronousEncryption/synchronousEncryption/SynchronousEncryptionApplicationTests.java
@@ -0,0 +1,13 @@
+package co.konradLorenz.synchronousEncryption.synchronousEncryption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynchronousEncryptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
